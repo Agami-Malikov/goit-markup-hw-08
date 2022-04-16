fd0a2684ff40fbeabc55992fb0a4f27925f7086a
Agami-Malikov <agami.malikov@gmail.com>
2022-04-16 23:37:35 +0300

    функционал конопки навигации

--- js/button.js
+++ js/button.js
@@ -13,3 +13,49 @@
     refs.modal.classList.toggle("header__navigation--active");
   }
 })();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-btn-on]"),
+    closeModalBtn: document.querySelector("[data-btn-off]"),
+    modal: document.querySelector("[data-btn1]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("--active");
+  }
+})();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-btn-on]"),
+    closeModalBtn: document.querySelector("[data-btn-off]"),
+    modal: document.querySelector("[data-btn2]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("--active");
+  }
+})();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-btn-on]"),
+    closeModalBtn: document.querySelector("[data-btn-off]"),
+    modal: document.querySelector("[data-btn3]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("--active");
+  }
+})();
+
